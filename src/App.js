e3c425ec3eba5bedc13b4011f7911cfacb3221bd
quang <21020230@vnu.edu.vn>
2022-04-21 20:29:44 +0700

    add login + signup (not set router)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./routes";
 import Navbar from "./components/Navbar/Navbar";
-
+import Signup from "./components/Signup/Signup";
+import Login from "./components/Login/Login";
 // css
 import "./App.css";
 import "./styles/font.css";
@@ -13,10 +14,14 @@ function App() {
       className="App"
       style={{ width: "100%", minHeight: "100vh", flexDirection: "column" }}
     >
-      <BrowserRouter>
+      
+       <BrowserRouter>
         <Navbar />
         <Router />
-      </BrowserRouter>
+        <Login/>
+        {/* <Signup/> */}
+      </BrowserRouter> 
+      
     </div>
   );
 }
---
+++ src/components/Login/Login.js
@@ -0,0 +1,25 @@
+import "./Login.css";
+const Login = () => {
+  return (
+    <div className="v9_242">
+      <div className="v9_243"></div>
+      <span className="v9_244">Sign In</span>
+      <span className="v9_245">Email Id</span>
+      <span className="v9_246">Don’t have account yet?</span>
+      <span className="v9_247">Password</span>
+      <span className="v9_248 forgot-password">Forgotten Password</span>
+      <div className="v9_249">
+        <input className="v9_250 login-field-text"></input>
+      </div>
+      <div className="v9_252">
+        <input className="v9_253 login-field-text"></input>
+      </div>
+      <button className="v9_255 login-big-button">
+            Create account
+      </button>
+      <button className="v9_256 login-small-button ">Login Now</button>
+      {/* <span className="v9_258">Create Account</span> */}
+    </div>
+  );
+};
+export default Login;
