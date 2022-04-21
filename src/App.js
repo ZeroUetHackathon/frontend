d5c5e8865fe1a38dd5f17b256d41349f57a44619
argargargtbn01 <21020230@vnu.edu.vn>
2022-04-21 11:17:27 +0700

    test signup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import "./styles/font.css";
 import Map from "./components/Map/Map";
+import Signup from "./components/Signup/Signup";
 function App() {
   return (
     <div className="App">
-      <Map />
+      {/* <Map /> */}
+      <Signup/>
     </div>
   );
 }
---
+++ src/components/Signup/Signup.js
@@ -0,0 +1,43 @@
+import "./Signup.css"
+const Signup = () => {
+    return (
+        <div class="v9_260">
+            <div class="v9_278">
+                <span class="v9_262">Sign Up</span>
+                <span class="v9_263">Email Id</span>
+                <span class="v9_265">Password</span>
+                <span class="v11_326">Confirm your password</span>
+                <div class="v9_267">
+                    <input class="v9_268 login-field-text"></input>
+
+                </div>
+                <div class="v9_270">
+                    <div class="v9_271"></div>
+                    <input class="v9_271 login-field-text" ></input>
+                </div>
+                <div class="v11_325">
+                    <div class="v9_274"></div>
+                    <button class="v9_274">
+                        <span class="v9_275">Create accout</span>
+                    </button>
+                    <button class="v9_273">
+                        <div class="v9_276">Sign In</div>
+                    </button>
+                </div>
+            </div>
+            <div class="v9_277">
+                <div class="v9_261"></div>
+                <span class="v9_264">You have an account ?</span>
+                <button class="v9_276 v9_273">
+                    Sign In
+                </button>
+            </div>
+            <div class="v11_322">
+                <div class="v11_323"></div>
+                <input class="v11_323 login-field-text"></input>
+            </div>
+        </div>
+    )
+}
+
+export default Signup;
